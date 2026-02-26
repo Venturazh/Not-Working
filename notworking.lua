32dbdae28572bdc113ec9611046dc5a9752e5465
Venturazh <venturazh.0@gmail.com>
2026-02-26 11:13:35 -0800

    Making Universal and reworking speedhack
    Added SpeedHack and Fly functionalities with toggles and keybinds.

--- notworking.lua
+++ notworking.lua
@@ -13,6 +13,7 @@ local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:FindFirstChildOfClass("Humanoid")
 local root = character:FindFirstChild("HumanoidRootPart")
 local camera = game.Workspace.CurrentCamera
+local animtor = humanoid:WaitForChild("Animator")
 
 local PlayerGui = player:WaitForChild("PlayerGui")
 
@@ -40,8 +41,8 @@ local Removals = Tabs.Main:AddLeftGroupbox('Removals')
 local Quality = Tabs.Main:AddRightGroupbox("Quality of Life")
 local PlayerEsp = Tabs.Visuals:AddLeftGroupbox("Player Esp")
 local NpcEsp = Tabs.Visuals:AddLeftGroupbox("Npc Esp")
-
-Movement:AddToggle("Fly", {
+if game.GameId == 4111023553 then
+    Movement:AddToggle("Fly", {
     Text = "Enable Fly",
     Default = false,
     Tooltip = "Enables Flying"
@@ -111,6 +112,99 @@ Movement:AddSlider("FlySpeed", {
 Options.FlySpeed:OnChanged(function()
     getgenv().FlySettings.Speed = Options.FlySpeed.Value
 end)
+
+local SpeedHack_Settings = {
+    Value = 0,
+    Active = false,
+    Saved = 0,
+}
+
+Movement:AddToggle("SpeedHack", {
+    Text = "Speed Hack",
+    Default = false,
+}):AddKeyPicker('SpeedHackKey', {
+    Default = 'nil',
+    SyncToggleState = true,
+    Mode = 'Toggle',
+    Text = 'Speedhack Keybind',
+    NoUI = false,
+})
+
+Toggles.SpeedHack:OnChanged(function()
+    SpeedHack_Settings.Active = Toggles.SpeedHack.Value
+    local Velo = root.AssemblyLinearVelocity
+    if SpeedHack_Settings.Active then
+        Connections.SpeedConn1 = RunService.Heartbeat:Connect(function()
+            if UserInputService:IsKeyDown(Enum.KeyCode.W) then
+                root.AssemblyLinearVelocity = root.CFrame.LookVector * SpeedHack_Settings.Value
+            end
+        end)
+    else
+        root.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+        if Connections.SpeedConn1 then
+            Connections.SpeedConn1:Disconnect()
+        end
+        SpeedHack_Settings.Value = Velo
+    end
+end)
+
+Movement:AddSlider("SpeedHackSpeed", {
+    Text = "SpeedHack Speed Multiplier",
+    Default = 25,
+    Min = 1, 
+    Max = 150,
+    Rounding = 1,
+    Compact = false
+})
+
+Options.SpeedHackSpeed:OnChanged(function()
+    SpeedHack_Settings.Value = Options.SpeedHackSpeed.Value
+end)
+
+Removals:AddToggle("invis", {
+    Text = "Invis",
+    Default = false,
+    Tooltip = "Toggles Invis"
+}):AddKeyPicker("invisKey", {
+    Default = 'nil',
+    SyncToggleState = true,
+    Mode = 'Toggle',
+    Text = 'InvisKeybind',
+    NoUI = false,
+})
+
+
+Toggles.invis:OnChanged(function()
+    local LiveFolder = game.Workspace:WaitForChild("Live")
+    local self = LiveFolder:WaitForChild(player.Name)
+
+    local SlideAnim = game:GetService("ReplicatedStorage").Assets.Anims.Movement.Slide
+    local Thing = animtor:LoadAnimation(SlideAnim)
+
+    if Toggles.invis.Value then
+
+        Connections.Invis = RunService.Heartbeat:Connect(function()
+
+            for i,v in pairs(player.Character.Humanoid:GetPlayingAnimationTracks()) do
+                v:Stop()
+            end
+
+            Thing:Play()
+        end)
+
+    else
+        if Connections.Invis then
+            Connections.Invis:Disconnect()
+        end
+
+        for i,v in pairs(player.Character.Humanoid:GetPlayingAnimationTracks()) do
+            v:Stop()
+        end
+
+        Thing:Stop()
+    end
+end)
+
 
 
 --============================ Removals ======================================================= 
@@ -210,7 +304,26 @@ Options.Distance:OnChanged(function()
     EspManager.sharedSettings.maxDistance = Options.Distance.Value
 end)
 
+
 local Npcs = {}
+
+local function UpdateEsp(alive)
+    local NpcObjectEsp = EspManager.AddInstance(alive, {
+        text = alive.Name .. " " .. "( " .. math.round(alive.Humanoid.Health) .. ' / ' .. alive.Humanoid.MaxHealth .. " ) " .. math.round((alive.Humanoid.Health / alive.Humanoid.MaxHealth) * 100) .. '%',
+        textColor = { Color3.new(0, 0.882353, 1), 1 },
+        limitDistance = true,
+        maxDistance = 400,
+    })
+
+    Connections.NpcEsp = RunService.Heartbeat:Connect(function()
+        if alive and alive:FindFirstChild("Humanoid") then
+            NpcObjectEsp.options.text = alive.Name .. " " .. "( " .. math.round(alive.Humanoid.Health) .. ' / ' .. alive.Humanoid.MaxHealth .. " ) " .. math.round((alive.Humanoid.Health / alive.Humanoid.MaxHealth) * 100) .. '%'
+        end
+    end)
+
+    table.insert(Npcs, NpcObjectEsp)
+end
+
 
 NpcEsp:AddToggle('Npc', {
     Text = 'Npc Esp',
@@ -225,52 +338,22 @@ Toggles.Npc:OnChanged(function()
             Liveplayers[oplayer.Name] = true
         end
 
-
         for _, alive in pairs(game.Workspace.Live:GetChildren()) do
             if not Liveplayers[alive.Name] then
-            -- if Connections.NpcEsp then continue end
-            
-            local NpcObjectEsp = EspManager.AddInstance(alive, {
-                text = alive.Name .. " " .. "( " .. math.round(alive.Humanoid.Health) .. ' / ' .. alive.Humanoid.MaxHealth .. " ) " .. math.round((alive.Humanoid.Health / alive.Humanoid.MaxHealth) * 100) .. '%',
-                textColor = { Color3.new(0, 0.882353, 1), 1 },
-                limitDistance = true,
-                maxDistance = 400,
-            })
-
-            Connections.NpcEsp = RunService.Heartbeat:Connect(function()
-                if alive and alive:FindFirstChild("Humanoid") then
-                    NpcObjectEsp.options.text = alive.Name .. " " .. "( " .. math.round(alive.Humanoid.Health) .. ' / ' .. alive.Humanoid.MaxHealth .. " ) " .. math.round((alive.Humanoid.Health / alive.Humanoid.MaxHealth) * 100) .. '%'
-                end
-            end)
-
-            table.insert(Npcs, NpcObjectEsp)
+                UpdateEsp(alive)
             end    
         end
 
 
-        
-
-        game.Workspace.Live.ChildAdded:Connect(function()
+        Connections.NpcEsp2 = game.Workspace.Live.ChildAdded:Connect(function()
             for _, alive in pairs(game.Workspace.Live:GetChildren()) do
                 if not Liveplayers[alive.Name] then
                 if EspManager._objectCache[alive] then return end
-                -- if Connections.NpcEsp2 then continue end
-
-                local NpcObjectEsp = EspManager.AddInstance(alive, {
-                    text = alive.Name .. " " .. "( " .. math.round(alive.Humanoid.Health) .. ' / ' .. alive.Humanoid.MaxHealth .. " ) " .. math.round((alive.Humanoid.Health / alive.Humanoid.MaxHealth) * 100) .. '%',
-                    textColor = { Color3.new(0, 0.882353, 1), 1 },
-                    limitDistance = true,
-                    maxDistance = 400,
-                })
-
-                Connections.NpcEsp2 = RunService.Heartbeat:Connect(function()
-                    if alive and alive:FindFirstChild("Humanoid") then
-                        NpcObjectEsp.options.text = alive.Name .. " " .. "( " .. math.round(alive.Humanoid.Health) .. ' / ' .. alive.Humanoid.MaxHealth .. " ) " .. math.round((alive.Humanoid.Health / alive.Humanoid.MaxHealth) * 100) .. '%'
+                    task.wait()
+                    if alive:FindFirstChild("Humanoid")  then
+                        UpdateEsp(alive)
                     end
-                end)
-
-                table.insert(Npcs, NpcObjectEsp)
-                end    
+                end
             end
         end)
     else
@@ -292,6 +375,173 @@ Toggles.Npc:OnChanged(function()
         end
     end
 end)
+else
+
+        Movement:AddToggle("Fly", {
+    Text = "Enable Fly",
+    Default = false,
+    Tooltip = "Enables Flying"
+}):AddKeyPicker("FlyKey", {
+    Default = 'nil',
+    SyncToggleState = true,
+    Mode = 'Toggle',
+    Text = 'Fly Keybind',
+    NoUI = false,
+})
+
+getgenv().FlySettings = {
+    Speed = 25
+}
+
+Toggles.Fly:OnChanged(function()
+    if Toggles.Fly.Value then
+        getgenv().FlySettings.LinearVelocity = Instance.new("LinearVelocity")
+        getgenv().FlySettings.Attachment = Instance.new("Attachment")
+
+        local LinearVelocity = getgenv().FlySettings.LinearVelocity
+        local Attachment = getgenv().FlySettings.Attachment
+
+        Connections.FlyConn = RunService.Heartbeat:Connect(function()
+            local move = Vector3.zero
+
+            if UserInputService:IsKeyDown(Enum.KeyCode.W) then move += camera.CFrame.LookVector end
+            if UserInputService:IsKeyDown(Enum.KeyCode.S) then move -= camera.CFrame.LookVector end
+            if UserInputService:IsKeyDown(Enum.KeyCode.A) then move -= camera.CFrame.RightVector end
+            if UserInputService:IsKeyDown(Enum.KeyCode.D) then move += camera.CFrame.RightVector end
+
+            if move.Magnitude > 0 then
+                move = move.Unit * getgenv().FlySettings.Speed
+            end
+
+            LinearVelocity.VectorVelocity = move
+        end)
+
+        Attachment.Parent = root
+        LinearVelocity.Parent = root
+        LinearVelocity.VelocityConstraintMode = Enum.VelocityConstraintMode.Vector
+        LinearVelocity.MaxForce = 100000
+        LinearVelocity.RelativeTo = Enum.ActuatorRelativeTo.World
+        LinearVelocity.Attachment0 = Attachment
+    else
+        if Connections.FlyConn then
+            Connections.FlyConn:Disconnect()
+        end
+        if getgenv().FlySettings.LinearVelocity then
+            FlySettings.LinearVelocity:Destroy()
+        end 
+        if FlySettings.Attachment then
+            getgenv().FlySettings.Attachment:Destroy()
+        end
+    end
+end)
+
+Movement:AddSlider("FlySpeed", {
+    Text = "Fly Speed",
+    Default = 50,
+    Min = 25,
+    Max = 150,
+    Rounding = 1,
+    Compact = false
+})
+
+Options.FlySpeed:OnChanged(function()
+    getgenv().FlySettings.Speed = Options.FlySpeed.Value
+end)
+
+local SpeedHack_Settings = {
+    Value = 0,
+    Active = false,
+    Saved = 0,
+}
+
+Movement:AddToggle("SpeedHack", {
+    Text = "Speed Hack",
+    Default = false,
+}):AddKeyPicker('SpeedHackKey', {
+    Default = 'nil',
+    SyncToggleState = true,
+    Mode = 'Toggle',
+    Text = 'Speedhack Keybind',
+    NoUI = false,
+})
+
+Toggles.SpeedHack:OnChanged(function()
+    SpeedHack_Settings.Active = Toggles.SpeedHack.Value
+    local Velo = root.AssemblyLinearVelocity
+    if SpeedHack_Settings.Active then
+        Connections.SpeedConn1 = RunService.Heartbeat:Connect(function()
+            if UserInputService:IsKeyDown(Enum.KeyCode.W) then
+                root.AssemblyLinearVelocity = root.CFrame.LookVector * SpeedHack_Settings.Value
+            end
+        end)
+    else
+        root.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+        if Connections.SpeedConn1 then
+            Connections.SpeedConn1:Disconnect()
+        end
+        SpeedHack_Settings.Value = Velo
+    end
+end)
+
+Movement:AddSlider("SpeedHackSpeed", {
+    Text = "SpeedHack Speed Multiplier",
+    Default = 25,
+    Min = 1, 
+    Max = 150,
+    Rounding = 1,
+    Compact = false
+})
+
+Options.SpeedHackSpeed:OnChanged(function()
+    SpeedHack_Settings.Value = Options.SpeedHackSpeed.Value
+end)
+
+    PlayerEsp:AddToggle("Esp", {
+    Text = "Esp",
+    Default = false,
+    Tooltip = "Enables Player Esp"
+}):AddKeyPicker("EspBind", {
+    Default = 'nil',
+    SyncToggleState = true,
+    Mode = 'Toggle',
+    Text = 'Enable Esp',
+    NoUI = false,
+})
+
+Toggles.Esp:OnChanged(function()
+    EspManager.teamSettings.enemy.enabled = Toggles.Esp.Value
+    EspManager.teamSettings.enemy.box = Toggles.Esp.Value
+    EspManager.teamSettings.enemy.name = Toggles.Esp.Value
+    EspManager.teamSettings.enemy.healthBar = Toggles.Esp.Value
+    EspManager.teamSettings.enemy.distance = Toggles.Esp.Value
+    EspManager.teamSettings.enemy.boxColor = { Color3.new(0.984314, 1, 0), 1 }
+    EspManager.teamSettings.enemy.boxOutlineColor = { Color3.new(0, 0, 0), 1 }
+end)
+
+PlayerEsp:AddToggle("Highlight", {
+    Text = "Highlight Player",
+    Default = false,
+    Tooltip = "Enables Player Highlight"
+})
+
+Toggles.Highlight:OnChanged(function()
+    EspManager.teamSettings.enemy.chams = Toggles.Highlight.Value
+end)
+
+PlayerEsp:AddSlider("Distance", {
+    Text = "Esp Distance",
+    Default = 10000,
+    Min = 1000,
+    Max = 20000,
+    Rounding = 1,
+    Compact = false
+})
+
+Options.Distance:OnChanged(function()
+    EspManager.sharedSettings.maxDistance = Options.Distance.Value
+end)
+
+end
 
 
 Library:SetWatermarkVisibility(false)
