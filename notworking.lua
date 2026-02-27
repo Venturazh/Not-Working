00aa55c9ee7dbad8ac0a677ba66b7752793aa1e6
Venturazh <venturazh.0@gmail.com>
2026-02-26 16:54:25 -0800

    Update notworking.lua

--- notworking.lua
+++ notworking.lua
@@ -41,7 +41,9 @@ local Removals = Tabs.Main:AddLeftGroupbox('Removals')
 local Quality = Tabs.Main:AddRightGroupbox("Quality of Life")
 local PlayerEsp = Tabs.Visuals:AddLeftGroupbox("Player Esp")
 local NpcEsp = Tabs.Visuals:AddLeftGroupbox("Npc Esp")
-if game.PlaceId == 4111023553 then
+
+if game.PlaceId == 6032399813 then
+    print("Hello World")
     Movement:AddToggle("Fly", {
     Text = "Enable Fly",
     Default = false,
